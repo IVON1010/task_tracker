eee0ab6d26d2495b05eaf90c734ea618eac06ad5
Yvonne <ndunguyvonne10@gmail.com>
2025-02-09 13:01:06 +0300

    define saving tasks

--- task_tracker.py
+++ task_tracker.py
@@ -1,9 +1,17 @@
 import json
-import os
+from pathlib import Path
 
-TASK_FILE = "tasks.json"
+TASK_FILE = Path("tasks.json")
 
-def initialize():
-    os.path.exists(TASK_FILE)
-    with open("TASK_FILE", mode="w") as file:
-        json.dumps([], file)
+def load_tasks():
+    if not TASK_FILE.exists():
+        return []
+    try:
+        with open(TASK_FILE, mode='r') as file:
+            return json.load(file)
+    except json.JSONDecodeError:
+        return []
+
+def save_tasks(tasks):
+    with open(TASK_FILE, mode='w') as file:
+        json.dumps(tasks, file, indent=4)
