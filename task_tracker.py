75fb2000354b742e22072b09cdc6af78d55335b6
Yvonne <ndunguyvonne10@gmail.com>
2025-02-09 13:19:52 +0300

    Adding timestamp to track any creatiionn and update of tasks

--- task_tracker.py
+++ task_tracker.py
@@ -1,6 +1,7 @@
 import json
 import uuid
 from pathlib import Path
+from datetime import datetime
 
 TASK_FILE = Path("tasks.json")
 
@@ -17,8 +18,12 @@ def save_tasks(tasks):
     with open(TASK_FILE, mode='w') as file:
         json.dumps(tasks, file, indent=4)
 
-def add_tasks():
+def add_tasks(description):
     tasks = load_tasks()
     task = {
-        "id": str.
+        "id": str(uuid.uuid4()),
+        "description": description,
+        "status": "Pending",
+        "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "Updated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
     }
